029385edde234500640229e9a86942e51e03f603
Tai Kedzierski <dch.tai@gmail.com>
2018-09-18 23:37:46 +0100

    further fleshing out of the parsing/rewriting

--- src/parse.py
+++ src/parse.py
@@ -1,2 +1,22 @@
+from lxml.html import parse as parseHTML
+import io
+
 def fix(pagedata, url):
-    return pagedata
+    dom = parseHTML(io.StringIO(pagedata) ).getroot()
+
+    for element_name in ["link", "a"]:
+        adjust(element_name, "href", url)
+
+    for element_name in ["img", "script", "object"]:
+        adjust(element_name, "src", url)
+
+    return str(dom)
+
+def adjust(element_name, attribute, url):
+    elements = dom.get_elements_by_name(element_name)
+
+    for elem in elements:
+        path = elem[attribute] # FIXME
+        if re.match("^/", path):
+            path = urlresolve(url, path)
+        elem[atribute] = serverurl+path # FIXME
