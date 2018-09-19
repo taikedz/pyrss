d05926f3ece170a8325d2689459b7f094b64fb3a
Tai Kedzierski <dch.tai@gmail.com>
2018-09-19 23:27:58 +0100

    adjust setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 faile() {
     echo "$*"
     exit 1
 }
 
+venvdir="${1:-venv}"
+
 [[ "$UID" != 0 ]] || [[ "$2" = -f ]] || faile "You must *not* be root to run thsi script. Use '-f' as second argument to override"
 
 sudo apt-get update && sudo apt-get install python3-venv || faile "Could not install pre-req packages"
 
-pyvenv "${1:-venv}" || faile "Could not run pyvenv"
+[[ -f "$venvdir/bin/activate" ]] || {
+    pyvenv "$venvdir" || faile "Could not run pyvenv to create '$venvdir' virtual environment"
 
-activation=(. "${1:-venv}/bin/activate")
-"${activation[@]}" || faile "Could not activate virtual environment"
+    activation=(. "$venvdir/bin/activate")
+    "${activation[@]}" || faile "Could not activate virtual environment"
 
-pip3 install flask cssselect || faile "pip install failed"
+    pip3 install flask cssselect || faile "pip install failed"
+}
 
 echo "You must now activate venv: \`${activation[*]}\`"
